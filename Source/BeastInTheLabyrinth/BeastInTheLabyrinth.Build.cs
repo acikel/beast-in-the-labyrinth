2b4f1fcd072a2caf8c327d61d496fc7ea4f5ed06
Aylin Acikel <aylincacikel@gmail.com>
2021-12-02 11:46:18 +0100

    Add fmod project files without plugin

--- Source/BeastInTheLabyrinth/BeastInTheLabyrinth.Build.cs
+++ Source/BeastInTheLabyrinth/BeastInTheLabyrinth.Build.cs
@@ -10,7 +10,7 @@ public class BeastInTheLabyrinth : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemUtils", "AudioMixer", "Synthesis" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "FMODStudio" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
